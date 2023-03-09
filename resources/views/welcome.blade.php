f225ed03724b202452ecda71429d5cb576f011d4
FontaMatte <matteo.fontanel@gmail.com>
2023-03-09 17:17:52 +0100

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+{{-- @extends('layout.main')
+
+@section('page_title')
+{{ $title }}
+@endsection --}}
+<img src="{{ Vite::asset('resources/img/adv.jpg') }}" alt="">
+
+{{-- @section('content') --}}
+
+
+
+{{-- @endsection --}}
